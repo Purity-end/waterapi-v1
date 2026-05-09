7e6e645e782c287e8d8056171b3dd4dd535023ce
Purity-end <2182626878@qq.com>
2026-05-09 10:33:34 +0800

    Delete install.R

--- install.R
+++
@@ -1,6 +0,0 @@
-# install.R
-options(repos = c(CRAN = "https://mirrors.tuna.tsinghua.edu.cn/CRAN/"))
-install.packages("plumber", quiet = TRUE)
-install.packages("randomForest", quiet = TRUE)
-install.packages("xgboost", quiet = TRUE)
-install.packages("glmnet", quiet = TRUE)
